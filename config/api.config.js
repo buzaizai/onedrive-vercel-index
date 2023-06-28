eff226dbbaa473f3936b69fc7e81ded196ac6a41
buzaizai <114451275+buzaizai@users.noreply.github.com>
2023-06-28 18:29:34 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'ac09809a-ffbc-4939-adde-a5b843a04f43'，
-  obfuscatedClientSecret: 'U2FsdGVkX1++rNMqdKnJGr3htYYUaH4qjqBP94SNzY/9AU9RWDB0HkP9nOEuSsnNL/LCvfuVcohcMuiu6IAmHg=='，
+  clientId: 'ac09809a-ffbc-4939-adde-a5b843a04f43',
+  obfuscatedClientSecret: 'U2FsdGVkX1++rNMqdKnJGr3htYYUaH4qjqBP94SNzY/9AU9RWDB0HkP9nOEuSsnNL/LCvfuVcohcMuiu6IAmHg==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
@@ -19,16 +19,16 @@ module.exports = {
 
   // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
   // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
-  authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token'，
-  driveApi: 'https://graph.microsoft.com/v1.0/me/drive'，
+  authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+  driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
 
   // The scope we require are listed here, in most cases you would not need to change this as well.
-  scope: 'user.read files.read.all offline_access'，
+  scope: 'user.read files.read.all offline_access',
 
   // Cache-Control header, check Vercel documentation for more details. The default settings imply:
   // - max-age=0: no cache for your browser
   // - s-maxage=0: cache is fresh for 60 秒之前 on the edge, after which it becomes stale
   // - stale-while-revalidate: allow serving stale content while revalidating on the edge
   // https://vercel.com/docs/c于cepts/edge-network/caching
-  cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate'，
+  cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
 }
